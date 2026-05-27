b3d5debeb9e64a18a44138dd29dbec779bd0137b
daxiazhanshenzhao <153145899+daxiazhanshenzhao@users.noreply.github.com>
2026-05-27 14:42:52 +0800

    Revert Beyond worldgen datagen

--- Beyond/src/main/java/org/galaxy/beyond/api/datagen/BeyondDataGenerator.java
+++ Beyond/src/main/java/org/galaxy/beyond/api/datagen/BeyondDataGenerator.java
@@ -4,10 +4,7 @@ import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.common.EventBusSubscriber;
 import net.neoforged.neoforge.data.event.GatherDataEvent;
 import org.galaxy.beyond.Beyond;
-import org.galaxy.beyond.api.datagen.custom.BeyondBiomeTagsProvider;
 import org.galaxy.beyond.api.datagen.custom.BeyondProgressGen;
-import org.galaxy.beyond.api.datagen.custom.BeyondStructureTagsProvider;
-import org.galaxy.beyond.api.datagen.custom.BeyondWorldgenProvider;
 
 @EventBusSubscriber(modid = Beyond.MODID)
 public class BeyondDataGenerator {
@@ -17,10 +14,7 @@ public class BeyondDataGenerator {
         var gen = event.getGenerator();
         var output = gen.getPackOutput();
 
-        event.createDatapackRegistryObjects(BeyondWorldgenProvider.BUILDER);
         var lookup = event.getLookupProvider();
         gen.addProvider(event.includeServer(), new BeyondProgressGen(output, lookup));
-        gen.addProvider(event.includeServer(), new BeyondBiomeTagsProvider(output, lookup, event.getExistingFileHelper()));
-        gen.addProvider(event.includeServer(), new BeyondStructureTagsProvider(output, lookup, event.getExistingFileHelper()));
     }
 }
